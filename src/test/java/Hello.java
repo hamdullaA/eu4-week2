97baf06e6bd38c3e0db1852ea19f5775323e4dce
Hamdulla.A <hamdulla3@gmail.com>
2021-02-11 23:33:35 +0100

    movie 2 branch watching on the phone 20 mins

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -2,5 +2,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("hello testing,eu41");
         //movie 2  on the phone 10 mins
+        //movie2 on the phone 20 mins
     }
 }
